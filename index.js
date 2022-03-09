cadcc40bfed2c7be590e75d258e48feaab6e2f6a
Bill Newman <william.newman@seattlecolleges.edu>
2022-03-09 08:46:04 +0000

    Adds EJS & index

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-var http = require("http");
-http.createServer(function(req,res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Aloha world');
-}).listen(process.env.PORT || 3000);
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.set('view engine','ejs');
+
+app.get('/', (req, res) => {
+  var title = "My NEW Home Page";
+  res.render('pages/index',{title,title})
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
